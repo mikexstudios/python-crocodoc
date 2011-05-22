03fe55bdb6bd58a001b93cc1696247c13b96c52c
Michael Huynh <mike.huynh@gmail.com>
2011-05-22 01:15:30 -0400

    First non-tested version of upload_url

--- crocodoc.py
+++ crocodoc.py
@@ -8,7 +8,19 @@ class Crocodoc(Resource):
         self.API_TOKEN = api_token
         super(Crocodoc, self).__init__(self.API_URL, **kwargs)
 
-    def upload():
+    def upload_url(url, title = None, async = False, private = False):
+        '''Upload and convert a file referenced by URL.'''
+        #NOTE: We may not want to pass params that are default.
+        r = self.get('document/upload', params_dict = {
+                'url': url,
+                'title': title, #may need to check if this is blank or not
+                'async': async,
+                'private': private,
+            })
+        return r
+
+    def upload_file(file, title = None, async = False, private = False):
+        '''Upload and convert a file uploaded via a POST request.'''
         pass
         
     def status():
