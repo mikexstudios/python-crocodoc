f97e313a930f6732ecdbc916bb6f56a76d6a768b
Michael Huynh <mike.huynh@gmail.com>
2011-05-24 23:56:26 -0400

    Added multiple status test (it also passes).

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 import unittest
 import crocodoc
 import os #for environ
+import time #for sleep (to rate limit)
+
+RATE_LIMIT_TIME = 5 #sec
 
 class TestCrocodoc(unittest.TestCase):
     sample_pdf_url = 'http://www.irs.gov/pub/irs-pdf/i1040ez.pdf'
@@ -10,7 +13,9 @@ class TestCrocodoc(unittest.TestCase):
 
     def tearDown(self):
         #TODO: Delete uploaded file.
-        pass
+
+        time.sleep(RATE_LIMIT_TIME) #sec, rate limits API requests
+
 
     def test_upload_url(self):
         r = self.crocodoc.upload(self.sample_pdf_url)
@@ -31,6 +36,23 @@ class TestCrocodoc(unittest.TestCase):
         self.assertTrue('status' in r)
         self.assertTrue('viewable' in r)
         self.assertTrue('uuid' in r)
+
+    def test_status_multiple(self):
+        #Upload multiple files
+        multiple = []
+        #crocodoc rate limits to 2 simultaneous conversions
+        for i in range(2):
+            multiple.append(self.crocodoc.upload(self.sample_pdf_url))
+
+        #Check the status on each file by querying all of the uuids at once
+        uuids = [uploaded['uuid'] for uploaded in multiple]
+
+        r = self.crocodoc.status(uuids)
+        self.assertTrue(len(r) == len(multiple))
+        for status in r:
+            self.assertTrue('status' in status)
+            self.assertTrue('viewable' in status)
+            self.assertTrue('uuid' in status)
 
 
 
