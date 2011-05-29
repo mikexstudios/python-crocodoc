e3bc0c026803251182e6721c69d02d28db7a3829
Michael Huynh <mike.huynh@gmail.com>
2011-05-28 22:41:41 -0400

    Added setup.py file.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'python-crocodoc',
+    packages = find_packages(),
+    version = '0.9.0',
+    description = "Simple wrapper around Crocodoc's API",
+    author = 'Michael Huynh',
+    author_email = 'mike@mikexstudios.com',
+    url = 'http://github.com/mikexstudios/python-crocodoc',
+    classifiers=[
+        'Programming Language :: Python', 
+        'License :: OSI Approved :: BSD License',
+    ]
+)
+
